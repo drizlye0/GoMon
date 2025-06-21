432d5e419eb17cbfa41ef64ff622a583b5bdfbee
drizlye0 <danielkastillo03@gmail.com>
2025-06-20 23:47:59 -0500

    feat: initial auth handler

---
+++ cmd/api/auth.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"net/http"
+
+	"github.com/drizlye0/GoMon/internal/store"
+	"github.com/google/uuid"
+)
+
+type RegisterUserPayload struct {
+	Username string `json:"username" validate:"required,min=4,max=32"`
+	Email    string `json:"email" validate:"required,email,max=255"`
+	Password string `json:"password" validate:"required,min=6,max=80"`
+}
+
+func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
+	var payload RegisterUserPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	if err := Validate.Struct(payload); err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := &store.User{
+		Username: payload.Username,
+		Email:    payload.Email,
+	}
+
+	if err := user.Password.Set(payload.Password); err != nil {
+		app.internalServerError(w, r, err)
+		return
+	}
+
+	plainToken := uuid.New().String()
+	hash := sha256.Sum256([]byte(plainToken))
+	hashToken := hex.EncodeToString(hash[:])
+
+}
